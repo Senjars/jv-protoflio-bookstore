b9be524c0f210d71d7572d6b1d11c0a697967498
Senjars <jaroslawsendecki1@gmail.com>
2026-01-03 16:26:54 +0100

    Initial commit

---
+++ src/test/java/io/github/senjar/bookstore_app/BookstoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.senjar.bookstore_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
