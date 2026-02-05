1dd75b8b77c92850884f98b01497dde22e84c23f
Senjars <jaroslawsendecki1@gmail.com>
2026-02-05 13:31:14 +0100

    LiquidBase fixes, automatic role assignment

--- src/main/java/io/github/senjar/bookstoreapp/service/UserServiceImpl.java
+++ src/main/java/io/github/senjar/bookstoreapp/service/UserServiceImpl.java
@@ -4,8 +4,12 @@ import io.github.senjar.bookstoreapp.dto.user.UserRegistrationRequestDto;
 import io.github.senjar.bookstoreapp.dto.user.UserResponseDto;
 import io.github.senjar.bookstoreapp.exception.RegistrationException;
 import io.github.senjar.bookstoreapp.mapper.UserMapper;
+import io.github.senjar.bookstoreapp.model.Role;
+import io.github.senjar.bookstoreapp.model.RoleName;
 import io.github.senjar.bookstoreapp.model.User;
+import io.github.senjar.bookstoreapp.repository.role.RoleRepository;
 import io.github.senjar.bookstoreapp.repository.user.UserRepository;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +19,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -25,6 +30,9 @@ public class UserServiceImpl implements UserService {
         }
 
         User user = userMapper.toModel(requestDto);
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER.name())
+                .orElseThrow(() -> new RegistrationException("Can't find role by name"));
+        user.setRoles(Set.of(userRole));
         User savedUser = userRepository.save(user);
 
         return userMapper.toResponseDto(savedUser);
